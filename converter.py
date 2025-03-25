d85944c9db552b1a9f0e0cf88fa69111dcdc3f89
igor-chlebowski <igorchlebowski@gmail.com>
2025-03-25 00:46:22 +0000

    Ale kurwa gówno

--- converter.py
+++ converter.py
@@ -1,1 +1,35 @@
-print("hello world")
+import cv2
+import numpy as np
+import utlis
+
+pathImage = "zd3.jpg"
+
+
+heightImg = 640
+widthImg = 480
+
+if __name__ == "__main__":
+    
+    img = cv2.imread(pathImage)
+    img = cv2.resize(img, (widthImg, heightImg))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #img = cv2.GaussianBlur(img, (5, 5), 1)
+    img = cv2.Canny(img, 100, 200)  # ustalone progi (można dostroić)
+
+    contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    biggest = utlis.biggestContour(contours)
+
+    if biggest.size != 0:
+        img = cv2.imread(pathImage)
+        img = cv2.resize(img, (widthImg, heightImg))
+        img = utlis.warpImg(img, biggest, widthImg, heightImg)
+    else:
+        img = np.zeros((heightImg, widthImg, 3), np.uint8)  # pusta grafika, jeśli brak konturu
+
+    cv2.imwrite("przetworzony.jpg", img)
+    
+    # Podgląd
+    #cv2.imshow("Kontury", imgContour)
+    #cv2.imshow("Canny", imgCanny)
+    
+    
---
+++ utlis.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+def biggestContour(contours):
+    biggest = np.array([])
+    max_area = 0
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 5000:
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+            if len(approx) == 4 and area > max_area:
+                biggest = approx
+                max_area = area
+    return biggest
+
+def reorder(points):
+    points = points.reshape((4, 2))
+    new_points = np.zeros((4, 1, 2), dtype=np.int32)
+    add = points.sum(1)
+    new_points[0] = points[np.argmin(add)]      # top-left
+    new_points[3] = points[np.argmax(add)]      # bottom-right
+    diff = np.diff(points, axis=1)
+    new_points[1] = points[np.argmin(diff)]     # top-right
+    new_points[2] = points[np.argmax(diff)]     # bottom-left
+    return new_points
+
+def warpImg(img, points, width, height):
+    reordered = reorder(points)
+    pts1 = np.float32(reordered)
+    pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    imgOutput = cv2.warpPerspective(img, matrix, (width, height))
+    return imgOutput
